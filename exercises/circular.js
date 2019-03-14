9dc8ece193a91ba049ddcd530f94bf2346c7210f
Guillaume Ribéroux <guillaume.riberoux@gmail.com>
2019-03-14 14:23:05 +0100

    update advanced exercices

--- exercises/circular.js
+++ exercises/circular.js
@@ -8,6 +8,9 @@
  */
 
 // Your code :
+const circular = new Object()
+circular.circular = circular
+
 
 //* Begin of tests
 const assert = require('assert')
--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -10,10 +10,21 @@
  *
  */
 
+function jadenCase(string){
+    let tab = string.split(" ");
+    for (let m in tab) {
+	    tab[m] = tab[m].substr(0, 1).toUpperCase()+tab[m].substr(1, tab[m].length);
+    }
+    string = tab.join(" ");
+    return(string);
+}
 
 
 //* Begin of tests
 const assert = require('assert')
 
-assert.fail('You must write your own tests')
+assert.strictEqual(typeof jadenCase, 'function')
+assert.strictEqual(jadenCase.length, 1)
+assert.strictEqual(jadenCase('coucou les gens'), 'Coucou Les Gens')
+// assert.fail('You must write your own tests')
 // End of tests */
